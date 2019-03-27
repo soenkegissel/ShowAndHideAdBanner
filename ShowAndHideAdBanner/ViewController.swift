78737d0999287deb027e51243c89fc85c22b902a
Sönke Gissel <soenke.gissel@gmail.com>
2019-03-27 15:16:13 +0100

    Initial Commit

---
+++ ShowAndHideAdBanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowAndHideAdBanner
+//
+//  Created by Sönke Gissel on 3/27/19.
+//  Copyright © 2019 Sönke Gissel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
